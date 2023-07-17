698f7d30e66114c0d26129c30a88d3d75907c9d1
Benjamin Louis <louisbenetval@orange.fr>
2023-07-17 20:50:19 +0200

    Commentaires (Création des Contrôlleurs)

--- src/Controller/EntrerpiseController.php
+++ src/Controller/EntrerpiseController.php
@@ -1,18 +1,18 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller;                                       // Grâce à la commande Symfony le contrôlleur se créer automatiquement
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class EntrerpiseController extends AbstractController
-{
-    #[Route('/entrerpise', name: 'app_entrerpise')]
-    public function index(): Response
+class EntrerpiseController extends AbstractController           // Permet d'accéder à des méthodes pré-établies dans l'AbstractController
+{                                                        
+    #[Route('/entrerpise', name: 'app_entrerpise')]             // Route représentant l'URL pour la redirection Veiller à ce que tout les name: soient différents
+    public function index(): Response                   
     {
-        return $this->render('entrerpise/index.html.twig', [
-            'controller_name' => 'EntrerpiseController',
+        return $this->render('entrerpise/index.html.twig', [    // render() Permet de faire le lien entre le controller et la view
+            'controller_name' => 'EntrerpiseController',        // Renvoi dans le dossier entreprise, dans le fichier index.html.twig
         ]);
     }
 }
