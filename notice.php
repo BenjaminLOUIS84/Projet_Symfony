aea9699b8203c2a4f7f089f2c34884995edd8286
Benjamin Louis <louisbenetval@orange.fr>
2023-07-17 20:37:38 +0200

    Notice explicative

--- notice.php
+++ notice.php
@@ -66,4 +66,50 @@ PS C:\laragon\www\symfony\Projet> symfony console doctrine:migrations:migrate (m
 <!-- 8 CREER LES CONTROLLEURS -->
 
 <!-- Dans la consôle: PS C:\laragon\www\symfony\Projet> symfony console make:controller: NomDuController (Ici EntrepriseControlleur) -->
- 
+
+<!-- COMMANDES SYMFONY -->
+<!-- 
+Créer un nouoveau projet
+symfony new NomDuProjet --full
+
+Lancer le serveur de Symfony
+symfony server:start 
+symfony serve
+symfony server:start -d (pour lancer le serveur en arrière-plan)
+symfony serve -d
+
+Stopper le serveur
+symfony server:stop
+
+Créer une nouvelle entity (ou mettre à jour une entity existante)
+symfony console make:entity (ou m:e)
+
+Créer la base de données
+symfony console doctrine:database:create
+
+Effectuer la migration (2 étapes !)
+symfony console make:migration (ou m:mi)
+symfony console doctrine:migrations:migrate (ou d:m:m)
+
+Mettre à jour la base de données après ajout d'un attribut dans une entité
+symfony console doctrine:schema:update --force (ou d:s:u --force)
+
+Créer un nouveau controller
+symfony console make:controller NomDuControlleur (ou m:con)
+
+Créer un nouveau form type
+symfony console make:form
+
+Vider le cache
+symfony console cache:clear
+
+Commandes liées à l'authentification
+symfony console make:user
+symfony console make:registration-form
+symfony console make:auth
+
+Vérification d'email + reset password
+composer require symfonycasts/verify-email-bundle
+composer require symfonycasts/reset-password-bundle
+symfony console make:reset-password
+-->
