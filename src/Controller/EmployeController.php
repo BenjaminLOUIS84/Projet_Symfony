b6875c1f4175d2837a22d9b3dfcb1fa5eb896a45
Benjamin Louis <louisbenetval@orange.fr>
2023-07-18 09:01:14 +0200

    Afficher la liste des employés

--- src/Controller/EmployeController.php
+++ src/Controller/EmployeController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;   // CF Commentaires de EntrepriseController
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Employe;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class EmployeController extends AbstractController
 {
     #[Route('/employe', name: 'app_employe')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+
+        $employes = $entityManager->getRepository(Employe::class)->findAll();
         return $this->render('employe/index.html.twig', [
-            'controller_name' => 'EmployeController',
+            'employes' => $employes
         ]);
     }
 }
