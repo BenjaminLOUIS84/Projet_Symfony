9dd1f4efd1389a6e54b2a8884e99c2ac8d7ede5a
Benjamin Louis <louisbenetval@orange.fr>
2023-07-18 20:46:56 +0200

    MAJ

--- src/Controller/EmployeController.php
+++ src/Controller/EmployeController.php
@@ -26,4 +26,17 @@ class EmployeController extends AbstractController
 
         ]);
     }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // FONCTION POUR AFFICHER LE DETAIL D'UN EMPLOYE
+
+    #[Route('/employe/{id}', name: 'show_employe')]       // Reprendre la route en ajoutant /{id} à l'URL et en changeant le nom du name
+
+    public function show(Employe $employe): Response       // Créer une fonction show() dans le controller pour afficher le détail d'un employé 
+
+    {
+        return $this->render('employe/show.html.twig', [     // Pour faire le lien entre le controller et la vue show.html.twig (il faut donc la créer dans le dossier employe)
+            'employe' => $employe
+        ]);
+    }
 }
