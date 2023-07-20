427595162994525e91876f87f0f9312f6a1b00be
Benjamin Louis <louisbenetval@orange.fr>
2023-07-20 22:03:51 +0200

    Bootstrap

--- notice.php
+++ notice.php
@@ -159,6 +159,18 @@ asset() fait référence au dossier public-->
 <!-- Même méthode pour lier un fichier js
 <script src="{{ asset('js/script.js') }}"></script> -->
 
+<!-- Ajouterle framework Bootstrap pour gagner du temps
+
+https://getbootstrap.com/
+
+{# Framework Bootstrap #}
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        
+{# Framework Bootstrap #}
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+        
+-->
+
 
 
 
