97789269e7b8f79f42020b6f6d53eb02a4e58503
Benjamin Louis <louisbenetval@orange.fr>
2023-07-20 22:15:42 +0200

    Fonctionnalité Bootstrap

--- notice.php
+++ notice.php
@@ -170,6 +170,12 @@ https://getbootstrap.com/
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
         
 -->
+<!-- Utiliser les fonctionnalités de Bootstrap -->
+<!-- [container] pour le main dans base.html.twig -->
+<!--  -->
+<!--  -->
+<!--  -->
+<!--  -->
 
 
 
